14e5c1f3f03a2dc3399be47b9620847efff718bf
elliotBraem <braemelliot@gmail.com>
2018-12-21 21:11:16 -0600

    test: tests for new empty survey

--- src/test/java/SurveyTest.java
+++ src/test/java/SurveyTest.java
@@ -1,26 +1,37 @@
+import br.com.devfast.jsurveymonkey.enums.StatusSurveyResponse;
 import br.com.devfast.jsurveymonkey.request.CreateSurveyRequest;
 import br.com.devfast.jsurveymonkey.response.CreateSurveyResponse;
 import br.com.devfast.jsurveymonkey.services.SurveyMonkeyService;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import utils.ConfigManager;
+
+import static org.junit.Assert.assertEquals;
 
 public class SurveyTest {
 
     private static final String API_AUTH_TOKEN;
+    private static SurveyMonkeyService surveyMonkeyService;
 
     static {
-        API_AUTH_TOKEN = ConfigManager.getProperty("api_auth_token");
+        API_AUTH_TOKEN = ConfigManager.getProperty("API_AUTH_TOKEN");
+    }
+
+    @BeforeClass
+    public static void beforeClassTests() {
+        surveyMonkeyService = new SurveyMonkeyService(API_AUTH_TOKEN);
     }
 
     @Test
     public void shouldCreateNewEmptySurvey() {
-        SurveyMonkeyService surveyMonkeyService = new SurveyMonkeyService(API_AUTH_TOKEN);
 
         CreateSurveyRequest createSurveyRequest = new CreateSurveyRequest();
         createSurveyRequest.setTitle("Test Survey");
         createSurveyRequest.setNickname("Test Survey Nickname");
+        createSurveyRequest.setAuthenticationToken(API_AUTH_TOKEN);
 
         CreateSurveyResponse createSurveyResponse = surveyMonkeyService.createSurvey(createSurveyRequest);
-        System.out.println(createSurveyResponse.getResponseStatus());
+        assertEquals(StatusSurveyResponse.SUCCESS, createSurveyResponse.getResponseStatus());
+
     }
 }
